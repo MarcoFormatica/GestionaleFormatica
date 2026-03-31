50f8eda124ea6779285aff2655d5e2ff7f9c4411
MarcoVLGE <140689432+MarcoVLGE@users.noreply.github.com>
2026-03-31 17:24:49 +0200

    Primo Commit

---
+++ GestionaleFormatica/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace GestionaleFormatica
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(27, 37);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 72;
+            dataGridView1.Size = new Size(1112, 338);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(44, 396);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Nome";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(122, 397);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(210, 35);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(46, 454);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 30);
+            label2.TabIndex = 3;
+            label2.Text = "Anni";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(122, 454);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(210, 35);
+            numericUpDown1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(46, 507);
+            button1.Name = "button1";
+            button1.Size = new Size(286, 40);
+            button1.TabIndex = 5;
+            button1.Text = "Aggiungi Utente";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(853, 397);
+            button2.Name = "button2";
+            button2.Size = new Size(286, 43);
+            button2.TabIndex = 6;
+            button2.Text = "Svuota Tabella";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1285, 665);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private NumericUpDown numericUpDown1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ GestionaleFormatica/Form1.cs
@@ -0,0 +1,85 @@
+using Microsoft.Data.Sqlite;
+using System.Data;
+
+
+namespace GestionaleFormatica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshTabella();
+        }
+
+        private void RefreshTabella()
+        {
+            var conn = CreateConnection();
+
+            DataTable dt = new DataTable();
+
+            SqliteCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM Users";
+
+
+            SqliteDataReader reader = cmd.ExecuteReader();
+            dt.Load(reader);
+            dataGridView1.DataSource = dt;
+        }
+
+        private static void AggiungiUtente(string nome, int anni)
+        {
+            SqliteConnection conn = CreateConnection();
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "INSERT INTO Users(Name,Age) VALUES (@name,@age)";
+            cmd.Parameters.AddWithValue("@name", nome);
+            cmd.Parameters.AddWithValue("@age", anni);
+            cmd.ExecuteNonQuery();
+        }
+
+        private static void CreaTabellaUsers()
+        {
+            SqliteConnection conn = CreateConnection();
+            SqliteCommand cmd = conn.CreateCommand();
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS Users(
+                                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                Name TEXT,
+                                Age INTEGER);";
+            cmd.ExecuteNonQuery();
+        }
+
+        private static SqliteConnection CreateConnection()
+        {
+            string connectionString = "Data Source=mydatabase.db";
+            SqliteConnection conn = new SqliteConnection(connectionString);
+            conn.Open();
+            return conn;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AggiungiUtente(textBox1.Text, (int)numericUpDown1.Value);
+            textBox1.Text = "";
+            numericUpDown1.Value = 0;
+            RefreshTabella();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SvuotaTabella();
+            RefreshTabella();
+        }
+
+        private void SvuotaTabella()
+        {
+            SqliteConnection conn = CreateConnection();
+            SqliteCommand cmd = conn.CreateCommand();
+            cmd.CommandText = @"DELETE FROM Users;";
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
